f72d10b877f23b49e1abcbedfb9eba94beafdb5b
Tamara Jordan <tamara.jordan@saltpay.co>
2022-08-28 13:54:17 +0100

    Add a basic string repr of a grid

--- game/game.go
+++ game/game.go
@@ -1,16 +1,66 @@
 package game
 
 import (
+	"strings"
+
 	"github.com/tamj0rd2/go-dots2/game/points"
 )
 
+const bottomCornerChar = "˙"
+const connectedSideChar = "|"
+
 type Game struct {
 	grid dots
+	size int
 }
 
 func New(size int) *Game {
 	dotCount := size * 4
-	return &Game{grid: make(dots, dotCount)}
+	return &Game{grid: make(dots, dotCount), size: size}
+}
+
+func (g *Game) Grid() string {
+	var lastIndex = g.size - 1
+	var out []string
+
+	for y := 0; y < g.size; y++ {
+		var (
+			tops    []string
+			mids    []string
+			bottoms []string
+
+			topCornerChar = ":"
+		)
+		if y == 0 {
+			topCornerChar = "."
+		}
+
+		for x := 0; x < g.size; x++ {
+			var (
+				top, mid, bottom string
+			)
+			top = topCornerChar + strings.Repeat("-", 3)
+			mid = connectedSideChar + strings.Repeat(" ", 3)
+			bottom = bottomCornerChar + strings.Repeat("-", 3)
+
+			if x == lastIndex {
+				top += topCornerChar
+				mid += connectedSideChar
+				bottom += bottomCornerChar
+			}
+
+			tops = append(tops, top)
+			mids = append(mids, mid)
+			bottoms = append(bottoms, bottom)
+		}
+
+		out = append(out, strings.Join(tops, ""), strings.Join(mids, ""))
+		if y == lastIndex {
+			out = append(out, strings.Join(bottoms, ""))
+		}
+	}
+
+	return strings.Join(out, "\n")
 }
 
 func (g *Game) Connect(a points.Coord, translation points.Translation) {
--- specifications/game_spec.go
+++ specifications/game_spec.go
@@ -1,6 +1,7 @@
 package specifications
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/alecthomas/assert/v2"
@@ -11,7 +12,7 @@ import (
 type GameDriver interface {
 	Connect(dot points.Coord, position points.Translation)
 	IsSquare(coordinate points.Coord) bool
-	// Grid() string
+	Grid() string
 }
 
 type Game struct {
@@ -31,11 +32,54 @@ func (spec Game) Test(t *testing.T) {
 		game.Connect(bottomRight, points.Left)
 
 		assert.True(t, game.IsSquare(points.Coord{X: 0, Y: 0}), "expected square to be owned")
-
-		/*bleh := `
-		 _  . . . .
-		|_| . . . .
-		. . . . . .`
-		*/
 	})
+
+	t.Run("connecting 3x3 squares in a single player game", func(t *testing.T) {
+		const size = 3
+
+		game := spec.NewSubject(size)
+		assert.False(t, game.IsSquare(points.Coord{X: 0, Y: 0}))
+
+		for y := 0; y < size; y++ {
+			for x := 0; x <size; x++ {
+				topLeft := points.Coord{X: 0, Y: 0}
+				bottomRight := points.Coord{X: 1, Y: 1}
+				game.Connect(topLeft, points.Right)
+				game.Connect(topLeft, points.Down)
+				game.Connect(bottomRight, points.Up)
+				game.Connect(bottomRight, points.Left)
+			}
+		}
+
+		assert.True(t, game.IsSquare(points.Coord{X: 0, Y: 0}), "expected square to be owned")
+		assertGridEquals(t, trim(`
+			.---.---.---.
+			|   |   |   |
+			:---:---:---:
+			|   |   |   |
+			:---:---:---:
+			|   |   |   |
+			˙---˙---˙---˙
+		`), game.Grid())
+	})
+}
+
+func trim(s string) string {
+	var out []string
+	s = strings.TrimSpace(s)
+	for _, s := range strings.Split(s, "\n") {
+		out = append(out, strings.TrimSpace(s))
+	}
+	return strings.Join(out, "\n")
+}
+
+func assertGridEquals(t testing.TB, expected, actual string) {
+	t.Helper()
+	expected = trim(expected)
+
+	if strings.EqualFold(expected, actual) {
+		return
+	}
+
+	t.Fatalf("expected grids to be equal\ngot:\n%s\nwant:\n%s", actual, expected)
 }
